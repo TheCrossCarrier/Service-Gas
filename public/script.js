6ce44657ee409295aea7eca04be3dd4a7fa84669
Daniel <danielthecrosscarier@gmail.com>
2021-04-13 09:31:34 +0300

    upd

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 const
-  root = document.querySelector(':root'),
   navbar = document.querySelector('.navbar'),
   navbarLogo = document.querySelector('.navbar .logo')
 
-/* Navbar Logo Scroll Transformations */
-
 onload = () => {
+
+  /* Navbar Logo Scroll Transformations */
+
   const
     navbarHeight = navbar.getBoundingClientRect().height,
     logoSize = navbarLogo.getBoundingClientRect().height
@@ -14,13 +14,43 @@ onload = () => {
     logoScale = navbarHeight / logoSize,
     logoTranslateY = (1 - logoScale) * logoSize / -2
 
-  onscroll = () => {
+  function logoTransform() {
     if (scrollY !== 0) {
       navbarLogo.style.transform = `scale(${logoScale})`
       navbarLogo.style.bottom = logoTranslateY + 'px'
     } else {
-      navbarLogo.style.transform = ''
-      navbarLogo.style.bottom = 0
+      navbarLogo.style.transform = 'none'
+      navbarLogo.style.bottom = '0'
     }
+  }
+
+  logoTransform()
+
+  onscroll = logoTransform
 }
-}
+
+/* Yandex Maps API */
+
+ymaps.ready(() => {
+
+  const
+
+    map = new ymaps.Map('map', {
+      center: [44.896350, 34.057387],
+      zoom: 16
+    })
+
+    placemark = new ymaps.Placemark(map.getCenter(), {
+      hintContent: 'ул. Озенбашская, 2А',
+      balloonContent: 'Service Gas Crimea'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/pin.svg',
+      iconImageSize: [70, 91],
+      iconImageOffset: [-30, -90]
+    })
+
+  map.geoObjects
+    .add(placemark)
+  
+})
